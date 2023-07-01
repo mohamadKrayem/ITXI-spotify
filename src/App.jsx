a1f44b09dd6ac387953813b1965486b9abebf172
Mohamad Krayem <mk2242682@gmail.com>
2023-07-01 22:10:24 +0300

    fixed the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Login from "./Pages/Login";
@@ -32,23 +28,23 @@ function App() {
   }
 
   async function getAccessTokenFromTheUrl(hash) {
-      let token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-      setAccessToken(token)
-      window.history.replaceState({}, document.title, "/")
-      window.localStorage.setItem("access_token", token)
-      return token
-   }
-
+    let token = hash
+      .substring(1)
+      .split("&")
+      .find((elem) => elem.startsWith("access_token"))
+      .split("=")[1];
+    setAccessToken(token);
+    window.history.replaceState({}, document.title, "/");
+    window.localStorage.setItem("access_token", token);
+    return token;
+  }
 
   useEffect(() => {
-      const hash = window.location.hash
-    if (
-      localStorage.getItem("access_token") === null && 
-      hash
-    ) {
-        getAccessTokenFromTheUrl(hash)
-      }
-    return () => {};
+    const hash = window.location.hash;
+    if (localStorage.getItem("access_token") === null && hash) {
+      getAccessTokenFromTheUrl(hash);
+    }
+    return () => { };
   }, [accessToken]);
 
   return (
@@ -58,15 +54,15 @@ function App() {
             bg-gray-400 p-3 text-black font-medium sm:pl-16 pl-8
          "
       >
-        <h1 className="text-2xl">Search in spotify</h1>
+        <h1 className="text-2xl">Spotify artist search</h1>
       </div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home accessToken={accessToken}/>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/artist/:id" element={<Artist />} />
-          </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home accessToken={accessToken} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/artist/:id" element={<Artist />} />
+        </Routes>
+      </Router>
     </>
   );
 }
